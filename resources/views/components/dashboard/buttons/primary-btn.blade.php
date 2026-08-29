711552c6d72162e82425246adc1623f9fe8b30bd
Adrian Thoenig <atoficial2016@gmail.com>
2026-08-29 16:32:36 +0200

    add primary btn

---
+++ resources/views/components/dashboard/buttons/primary-btn.blade.php
@@ -0,0 +1,8 @@
+@props([
+    'url' => '/dashboard',
+    'title' => 'Add new client'
+])
+
+<a href="{{ $url }}" class="block px-4 py-3 bg-gray-700 hover:bg-gray-600 rounded-lg text-white flex items-center gap-2">
+    {{ $title }}
+</a>
