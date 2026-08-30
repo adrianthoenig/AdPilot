439b2a993219f89075a07c701f59ccdbd61017d1
Adrian Thoenig <atoficial2016@gmail.com>
2026-08-30 15:32:14 +0200

    change to label structure

--- resources/views/components/inputs/radio.blade.php
+++ resources/views/components/inputs/radio.blade.php
@@ -8,20 +8,19 @@
     'message' => null
 ])
 
-<input 
-    type="radio" 
-    id="{{ $id }}" 
-    name="{{ $name }}" 
-    value="{{ $value }}" checked
-    class="peer"
->
-    <div class="cursor-pointer p-3 flex flex-col gap-2 bg-gray-100 peer-checked:bg-gray-200 hover:bg-gray-200 rounded-lg">
+
+
+<label for="{{ $id }}">
+    <input type="radio" id="{{ $id }}" name="{{ $name }}" value="{{ $value }}" checked class="peer appearance-none">
+    <div
+        class="cursor-pointer p-3 flex flex-col gap-2 bg-gray-100 peer-checked:bg-gray-200 hover:bg-gray-200 rounded-lg">
         <div class="flex gap-2 items-center">
             @if($icon)<i class="{{ $icon }}"></i>@endif
             <h4 class="text-lg">{{ $title }}</h4>
         </div>
         @if($message)
-        <p class="text-gray-400">Your client is still in the onboarding phase</p>
+            <p class="text-gray-400">Your client is still in the onboarding phase</p>
         @endif
     </div>
-</input>
+    </input>
+</label>
