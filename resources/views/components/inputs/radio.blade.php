9beaa05e0882f7ce84290186bcd0881d632ee1fb
Adrian Thoenig <atoficial2016@gmail.com>
2026-08-30 15:29:40 +0200

    add peer checked

--- resources/views/components/inputs/radio.blade.php
+++ resources/views/components/inputs/radio.blade.php
@@ -13,9 +13,9 @@
     id="{{ $id }}" 
     name="{{ $name }}" 
     value="{{ $value }}" checked
-    class=""
+    class="peer"
 >
-    <div class="cursor-pointer p-3 flex flex-col gap-2 bg-gray-100 hover:bg-gray-200 rounded-lg">
+    <div class="cursor-pointer p-3 flex flex-col gap-2 bg-gray-100 peer-checked:bg-gray-200 hover:bg-gray-200 rounded-lg">
         <div class="flex gap-2 items-center">
             @if($icon)<i class="{{ $icon }}"></i>@endif
             <h4 class="text-lg">{{ $title }}</h4>
