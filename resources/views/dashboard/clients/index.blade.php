0e3e7cb0ae5e32a6c9dba4e54a0130306d308628
Adrian Thoenig <atoficial2016@gmail.com>
2026-08-30 15:04:15 +0200

    render clients on index

--- resources/views/dashboard/clients/index.blade.php
+++ resources/views/dashboard/clients/index.blade.php
@@ -2,9 +2,17 @@
     <x-dashboard.main title="All clients">
         <x-dashboard.buttons.primary-btn title="Add new client" url="{{ route('dashboard.clients.create') }}" />
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
-            <x-dashboard.client-card />
-            <x-dashboard.client-card />
-            <x-dashboard.client-card />
+            @forelse($clients as $client)
+            <x-client-card
+                status="{{ $client->status }}"
+                clientName="{{ $client->name }}"
+                monthlyBudget="{{ $client->monthly_budget }}"
+                currency="{{ $client->currency }}"
+                joinedDate="{{ $client->created_at }}"
+            />
+            @empty
+            <p>No clients!</p>
+            @endforelse
         </div>
     </x-dashboard.main>
 </x-dashboard-layout>
