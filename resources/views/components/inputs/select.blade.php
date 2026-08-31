d84580c532118c648710a4c62e58c9435d2b59fb
Adrian Thoenig <atoficial2016@gmail.com>
2026-08-31 10:39:42 +0200

    add flash errors to select

--- resources/views/components/inputs/select.blade.php
+++ resources/views/components/inputs/select.blade.php
@@ -12,9 +12,13 @@
         <label class="text-base" for="{{ $id }}">{{ $label }}{{ $required ? '*' : '' }}</label>
     @endif
     <select id="{{ $id }}" name="{{ $name }}" value="{{ $value }}"
-        class="px-2 py-4 rounded-lg border border-gray-300 focus:outline-none" {{ $required ? 'required' : '' }}>
+        class="px-2 py-4 rounded-lg border border-gray-300 focus:outline-none @error($name) border-2 border-red-500 @enderror" 
+        {{ $required ? 'required' : '' }}>
         @foreach($options as $id => $value)
             <option id="{{ $id }}" value="{{ $id }}">{{ $value }}</option>
         @endforeach
     </select>
+    @error($name)
+    <p class="text-sm text-red-500">{{ $message }}</p>
+    @enderror
 </div>
