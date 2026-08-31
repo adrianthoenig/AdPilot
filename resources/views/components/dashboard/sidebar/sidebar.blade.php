0273f0f548f6b349d5b066345c4473a973fc24be
Adrian Thoenig <atoficial2016@gmail.com>
2026-08-31 12:42:46 +0200

    fix dashboard active state in sidebar

--- resources/views/components/dashboard/sidebar/sidebar.blade.php
+++ resources/views/components/dashboard/sidebar/sidebar.blade.php
@@ -3,7 +3,7 @@
     <div class="sticky top-4 gap-12 flex flex-col">
         <div class="flex flex-col gap-8">
             <x-dashboard.sidebar.sidebar-group>
-                <x-dashboard.sidebar.sidebar-button icon="briefcase" />
+                <x-dashboard.sidebar.sidebar-button icon="briefcase" :active="request()->is('dashboard')" />
                 <x-dashboard.sidebar.sidebar-button 
                     icon="user-group" 
                     url="{{ route('dashboard.clients.index') }}" 
