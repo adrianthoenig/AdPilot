198e2c159c05683d4cd787c9d9c04fb1de896778
Adrian Thoenig <atoficial2016@gmail.com>
2026-08-31 10:35:07 +0200

    add text input error flash

--- resources/views/components/inputs/text.blade.php
+++ resources/views/components/inputs/text.blade.php
@@ -18,7 +18,10 @@
         name="{{ $name }}" 
         placeholder="{{ $placeholder }}" 
         value="{{ $value }}"
-        class="px-2 py-4 rounded-lg border border-gray-300 focus:outline-none"
+        class="px-2 py-4 rounded-lg border border-gray-300 focus:outline-none @error($name) border-2 border-red-500 @enderror"
         {{ $required ? 'required' : '' }}
     />
+    @error($name)
+    <p class="text-sm text-red-500">{{ $message }}</p>
+    @enderror
 </div>
