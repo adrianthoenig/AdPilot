fbc526937400436214c6423321183e876b719fbb
Adrian Thoenig <atoficial2016@gmail.com>
2026-09-01 11:21:45 +0200

    finish currency formatter

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -2,32 +2,101 @@
 
 if (!function_exists('formatCurrency')) {
 
-    function formatCurrency(string $monthly_budget = '0', string $currencyCode = 'EUR'): string
+    function formatCurrency(string $monthly_budget = '0.00', string $currencyCode = 'EUR'): string
     {
         $symbols = [
-            'USD' => '$',
-            'EUR' => '€',
-            'GBP' => '£',
-            'CHF' => 'CHF',
-            'IRR' => '﷼',
-            'CAD' => '$',
-            'AUD' => '$',
-            'NZD' => '$',
-            'JPY' => '¥',
-            'CNY' => '¥',
-            'INR' => '₹',
-            'SGD' => 'S$',
-            'HKD' => 'HK$',
-            'AED' => 'AED',
-            'SAR' => '⃁',
-            'SEK' => 'kr',
-            'NOK' => 'kr',
-            'DKK' => 'kr.',
-            'MXN' => 'Mex$',
-            'BRL' => 'R$',
-            'PLN' => 'zł'
+            'USD' => [
+                'symbol' =>'$',
+                'placement' => 'before'
+            ],
+            'EUR' => [
+                'symbol' =>'€',
+                'placement' => 'after'
+            ],
+            'GBP' => [
+                'symbol' =>'£',
+                'placement' => 'before'
+            ],
+            'CHF' => [
+                'symbol' =>'CHF',
+                'placement' => 'before'
+            ],
+            'IRR' => [
+                'symbol' =>'﷼',
+                'placement' => 'after'
+            ],
+            'CAD' => [
+                'symbol' =>'$',
+                'placement' => 'before'
+            ],
+            'AUD' => [
+                'symbol' =>'$',
+                'placement' => 'before'
+            ],
+            'NZD' => [
+                'symbol' =>'$',
+                'placement' => 'before'
+            ],
+            'JPY' => [
+                'symbol' =>'¥',
+                'placement' => 'before'
+            ],
+            'CNY' => [
+                'symbol' =>'¥',
+                'placement' => 'before'
+            ],
+            'INR' => [
+                'symbol' =>'₹',
+                'placement' => 'before'
+            ],
+            'SGD' => [
+                'symbol' =>'S$',
+                'placement' => 'before'
+            ],
+            'HKD' => [
+                'symbol' =>'HK$',
+                'placement' => 'before'
+            ],
+            'AED' => [
+                'symbol' =>'AED',
+                'placement' => 'before'
+            ],
+            'SAR' => [
+                'symbol' =>'⃁',
+                'placement' => 'before'
+            ],
+            'SEK' => [
+                'symbol' =>'kr',
+                'placement' => 'after'
+            ],
+            'NOK' => [
+                'symbol' =>'kr',
+                'placement' => 'after'
+            ],
+            'DKK' => [
+                'symbol' =>'kr.',
+                'placement' => 'after'
+            ],
+            'MXN' => [
+                'symbol' =>'Mex$',
+                'placement' => 'before'
+            ],
+            'BRL' => [
+                'symbol' =>'R$',
+                'placement' => 'before'
+            ],
+            'PLN' => [
+                'symbol' =>'zł',
+                'placement' => 'after'
+            ]
+
         ];
 
-        return "{$monthly_budget} {$symbols[$currencyCode]}";
+        // Check placement
+        if($symbols[$currencyCode]['placement'] == 'before') {
+            return "{$symbols[$currencyCode]['symbol']}{$monthly_budget}";
+        }
+
+        return "{$monthly_budget}{$symbols[$currencyCode]['symbol']}";
     }
 }
