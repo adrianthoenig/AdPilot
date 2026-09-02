beda92438fb3eb2ecf694f0381ff3f1680a7d29a
Adrian Thoenig <atoficial2016@gmail.com>
2026-09-02 11:52:08 +0200

    finish custom client seeder

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -14,21 +14,10 @@ class ClientSeeder extends Seeder
      */
     public function run(): void
     {
-        Client::create([
-            'name' => 'Ecom Growth Partners',
-            'industry' => 'marketing_advertising',
-            'website' => 'https://www.ecomgrowthpartners.com/',
-            'country' => 'Spain',
-            'city' => 'Madrid',
-            'logo_path' => 'logos/egp.jpg',
-            'contact_name' => 'Carlos Higuera',
-            'email' => 'carlos@ecomgrowthpartners.com',
-            'job_title' => 'cmo',
-            'client_status' => 'active',
-            'start_date' => Carbon::now(),
-            'currency' => 'EUR',
-            'monthly_budget' => 40.000,
-            'advertising_platforms' => 'meta'
-        ]);
+        $clients = include database_path('data_loaders/clients.php');
+        foreach($clients as $client) {
+            Client::create($client);
+        }
+
     }
 }
