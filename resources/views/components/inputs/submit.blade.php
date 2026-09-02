197e9d7e433d6ba25ece49c7b4da317e0487176f
Adrian Thoenig <atoficial2016@gmail.com>
2026-09-02 15:10:17 +0200

    add transition effects to hover on primary buttons

--- resources/views/components/inputs/submit.blade.php
+++ resources/views/components/inputs/submit.blade.php
@@ -4,7 +4,7 @@
 
 <button 
     type="submit"
-    class="cursor-pointer flex items-center justify-center gap-2 px-4 py-3 bg-primary-400 hover:bg-primary-500 rounded-lg text-white flex items-center gap-2"
+    class="cursor-pointer flex items-center justify-center gap-2 px-4 py-3 bg-primary-400 hover:bg-primary-500 transition-colors rounded-lg text-white flex items-center gap-2"
     >
 {{ $title }}
 </button>
