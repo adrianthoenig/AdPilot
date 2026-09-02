6c4ca2aa742c5f12a6828df44d429761928da43f
Adrian Thoenig <atoficial2016@gmail.com>
2026-09-02 15:02:23 +0200

    add hover effect to action button

--- resources/views/components/dashboard/action-button.blade.php
+++ resources/views/components/dashboard/action-button.blade.php
@@ -3,6 +3,6 @@
     'icon' => 'eye'
 ])
 
-<a href="{{ $url }}" class="bg-primary-200 hover:bg-primary-300 rounded-lg px-4 py-3">
+<a href="{{ $url }}" class="bg-primary-200 hover:bg-primary-300 transition-colors rounded-lg px-4 py-3">
     <i class="fas fa-{{ $icon }} text-primary-900"></i>
 </a>
