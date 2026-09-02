687036208bee7aa2160eb86de773ae23b3162113
Adrian Thoenig <atoficial2016@gmail.com>
2026-09-02 11:47:15 +0200

    add fake clients array

---
+++ app/Support/clients.php
@@ -0,0 +1,70 @@
+<?php
+
+use Carbon\Carbon;
+
+return [
+    [
+        'name' => 'Ecom Growth Partners',
+        'industry' => 'marketing_advertising',
+        'website' => 'https://www.ecomgrowthpartners.com/',
+        'country' => 'Spain',
+        'city' => 'Madrid',
+        'logo_path' => 'logos/egp.jpg',
+        'contact_name' => 'Carlos Higuera',
+        'email' => 'carlos@ecomgrowthpartners.com',
+        'job_title' => 'cmo',
+        'client_status' => 'active',
+        'start_date' => Carbon::now(),
+        'currency' => 'EUR',
+        'monthly_budget' => 40.000,
+        'advertising_platforms' => 'google'
+    ],
+    [
+        'name' => 'Freshly Cosmetics',
+        'industry' => 'beauty',
+        'website' => 'https://www.freshlycosmetics.com/',
+        'country' => 'Spain',
+        'city' => 'Barcelona',
+        'logo_path' => 'logos/freshly.jpeg',
+        'contact_name' => 'Miquel Antolín',
+        'email' => 'miquel@freshlycosmetics.com',
+        'job_title' => 'owner',
+        'client_status' => 'active',
+        'start_date' => Carbon::now(),
+        'currency' => 'EUR',
+        'monthly_budget' => 250.000,
+        'advertising_platforms' => 'linkedin'
+    ],
+    [
+        'name' => 'B3TTER',
+        'industry' => 'food',
+        'website' => 'https://b3tterfoods.com/',
+        'country' => 'Spain',
+        'city' => 'Barcelona',
+        'logo_path' => 'logos/b3tter.png',
+        'contact_name' => 'Alex Boisset',
+        'email' => 'alex@ecomgrowthpartners.com',
+        'job_title' => 'founder',
+        'client_status' => 'active',
+        'start_date' => Carbon::now(),
+        'currency' => 'EUR',
+        'monthly_budget' => 42.000,
+        'advertising_platforms' => 'tiktok'
+    ],
+    [
+        'name' => 'Nude Project',
+        'industry' => 'fashion',
+        'website' => 'https://nude-project.com/',
+        'country' => 'Spain',
+        'city' => 'Barcelona',
+        'logo_path' => 'logos/nude.jpg',
+        'contact_name' => 'Alex Benlloch',
+        'email' => 'alex@nude-project.com',
+        'job_title' => 'ceo',
+        'client_status' => 'active',
+        'start_date' => Carbon::now(),
+        'currency' => 'EUR',
+        'monthly_budget' => 150.000,
+        'advertising_platforms' => 'meta'
+    ],
+];
