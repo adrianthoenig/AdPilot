8de4fbe98a28326655fc3c33dd5c91f2f617e1f7
Adrian Thoenig <atoficial2016@gmail.com>
2026-09-02 10:49:36 +0200

    add client show view

---
+++ resources/views/dashboard/clients/show.blade.php
@@ -0,0 +1,5 @@
+<x-dashboard-layout>
+    <x-dashboard.main title="Client name">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto recusandae aspernatur consectetur molestias vel laudantium quaerat eligendi. Rem, iste quas.</p>
+    </x-dashboard.main>
+</x-dashboard-layout>
