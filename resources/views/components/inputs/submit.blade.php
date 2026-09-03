8627bbdd3d59d7947e01cdb9ecfe41647580727f
Adrian Thoenig <atoficial2016@gmail.com>
2026-09-03 12:12:38 +0200

    disable button

--- resources/views/components/inputs/submit.blade.php
+++ resources/views/components/inputs/submit.blade.php
@@ -1,10 +1,21 @@
 @props([
-    'title' => 'Add new'
+    'title' => 'Add new',
+    'disabled' => true
 ])
 
+@if($disabled)
+<button 
+    type="submit"
+    disabled
+    class="flex items-center justify-center gap-2 px-4 py-3 bg-primary-200 transition-colors rounded-lg text-white flex items-center gap-2"
+    >
+{{ $title }}
+</button>
+@else
 <button 
     type="submit"
     class="cursor-pointer flex items-center justify-center gap-2 px-4 py-3 bg-primary-400 hover:bg-primary-500 transition-colors rounded-lg text-white flex items-center gap-2"
     >
 {{ $title }}
 </button>
+@endif
