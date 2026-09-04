bf0e92e55a3f91caae1437b62ff999bb911daece
Adrian Thoenig <atoficial2016@gmail.com>
2026-09-04 10:48:03 +0200

    remove freedom of width to summary

--- resources/views/components/dashboard/forms/summary.blade.php
+++ resources/views/components/dashboard/forms/summary.blade.php
@@ -7,38 +7,38 @@
 
 @vite('resources/js/modules/clientSummary.js')
 
-<div class="bg-white p-6 rounded-xl flex flex-col gap-6 sticky top-4">
+<div class="bg-white p-6 rounded-xl flex flex-col gap-6 sticky top-4 overflow-hidden">
     <div class="p-6 rounded-xl bg-gray-100 flex flex-col gap-6">
         <h4 class="text-xl">{{ $summaryTitle }}</h4>
-        <div class="flex flex-col gap-4">
+        <div class="flex flex-col gap-4 w-80 text-ellipsis overflow-hidden">
 
             {{-- Company name --}}
             <div class="flex items-center justify-between gap-25">
-                <p class="text-gray-600">Company name</p>
-                <p id="company-name-value"><span class="text-gray-400 animate-pulse">Pending</span></p>
+                <p class="text-gray-600 whitespace-nowrap">Company name</p>
+                <p id="company-name-value" class="text-ellipsis"><span class="text-gray-400 animate-pulse">Pending</span></p>
             </div>
 
             {{-- Client Status --}}
             <div class="flex items-center justify-between gap-25">
-                <p class="text-gray-600">Client status</p>
-                <p id="client-status-value"><span class="text-gray-400 animate-pulse">Pending</span></p>
+                <p class="text-gray-600 whitespace-nowrap">Client status</p>
+                <p id="client-status-value" class="text-ellipsis"><span class="text-gray-400 animate-pulse">Pending</span></p>
             </div>
 
             {{-- Total fields (DYNAMIC) --}}
             <div class="flex items-center justify-between gap-25">
-                <p class="text-gray-600">Total fields</p>
-                <p><span id="total-fields-filled">0</span> / <span id="total-fields-value">13</span></p>
+                <p class="text-gray-600 whitespace-nowrap">Total fields</p>
+                <p><span id="total-fields-filled" class="text-ellipsis">0</span> / <span id="total-fields-value">13</span></p>
             </div>
 
             {{-- Required (DYNAMIC) --}}
             <div class="flex items-center justify-between gap-25">
-                <p class="text-gray-600">Required fields</p>
+                <p class="text-gray-600 whitespace-nowrap">Required fields</p>
                 <p><span id="required-fields-filled" class="text-red-500">0</span> / <span id="required-fields-value">4</span></p>
             </div>
 
             {{-- Status --}}
             <div class="flex items-center justify-between gap-25">
-                <p class="text-gray-600">Status</p>
+                <p class="text-gray-600 whitespace-nowrap">Status</p>
                 <p id="status-value"><span class="text-red-500 animate-pulse">Not ready</span></p>
             </div>
         </div>
