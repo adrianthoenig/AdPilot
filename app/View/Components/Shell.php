69a70710b5ec35207e3da4d2a4bf5bacd4cedf09
Adrian Thoenig <atoficial2016@gmail.com>
2026-09-08 10:46:13 +0200

    fix dashboard shell

--- app/View/Components/Shell.php
+++ app/View/Components/Shell.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class ClientShell extends Component
+class Shell extends Component
 {
     /**
      * Create a new component instance.
@@ -21,6 +21,6 @@ class ClientShell extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.dashboard.clients.shell');
+        return view('components.shell');
     }
 }
--- resources/views/dashboard/clients/index.blade.php
+++ resources/views/dashboard/clients/index.blade.php
@@ -1,13 +1,13 @@
 <x-dashboard-layout>
     <x-dashboard.main title="All clients">
-        <x-dashboard.clients.shell />
+        <x-dashboard.shell>
+            <x-dashboard.buttons.primary-btn title="Add new client" url="{{ route('dashboard.clients.create') }}" />
+        </x-dashboard.shell>
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
             @forelse($clients as $client)
-                <x-client-card
-                    :client="$client"
-                />
+                <x-client-card :client="$client" />
             @empty
-            <p>No clients!</p>
+                <p>No clients!</p>
             @endforelse
         </div>
     </x-dashboard.main>
