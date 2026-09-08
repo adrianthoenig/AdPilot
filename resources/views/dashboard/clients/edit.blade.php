2f243925e70bb5e791cb03890b5b9e30632f84dd
Adrian Thoenig <atoficial2016@gmail.com>
2026-09-08 10:57:23 +0200

    add client text input values

---
+++ resources/views/dashboard/clients/edit.blade.php
@@ -0,0 +1,286 @@
+<x-dashboard-layout>
+    <x-dashboard.main title="Edit client">
+            <form action="/dashboard/clients/" method="POST" id="create-client-form" class="flex items-start gap-6" enctype="multipart/form-data">
+                @csrf
+                <div class="bg-white flex-1 p-6 rounded-3xl flex flex-col gap-8">
+                    <x-dashboard.forms.form-block label="Company information">
+                        <x-inputs.text 
+                        id="name" 
+                        name="name" 
+                        placeholder="MogulZ Media" 
+                        label="Company Name" 
+                        required="true"
+                        maxlength="120"
+                        value="{{ $client->name }}"
+                        />
+
+                        <x-inputs.select 
+                        id="industry" 
+                        name="industry" 
+                        label="Industry"
+                        default="Select your clients industry"
+                        :options="[
+                                'automotive' => 'Automotive',
+                                'beauty' => 'Beauty',
+                                'construction_real_estate' => 'Construction & Real Estate',
+                                'consulting' => 'Consulting',
+                                'education' => 'Education',
+                                'ecommerce' => 'Ecommerce',
+                                'fashion' => 'Fashion',
+                                'finance' => 'Finance',
+                                'fitness' => 'Fitness',
+                                'food' => 'Food',
+                                'healthcare' => 'Healthcare',
+                                'home_interior' => 'Home & Interior',
+                                'hospitality_travel' => 'Hospitality & Travel',
+                                'legal_services' => 'Legal Services',
+                                'manufacturing' => 'Manufacturing',
+                                'marketing_advertising' => 'Marketing & Advertising',
+                                'media_entertainment' => 'Media & Entertainment',
+                                'nonprofit' => 'Nonprofit',
+                                'pet_care' => 'Pet Care',
+                                'professional_services' => 'Professional Services',
+                                'restaurants_cafes' => 'Restaurants & Cafes',
+                                'saas_tech' => 'SaaS & Technology',
+                                'sports' => 'Sports',
+                                'telecommunications' => 'Telecommunications',
+                                'other' => 'Other'
+                            ]"/>
+
+                        <x-inputs.text 
+                        id="website" 
+                        name="website" 
+                        type="url"
+                        placeholder="mogulzmedia.com" 
+                        label="Website" 
+                        maxlength="255"
+                        value="{{ $client->website }}"
+                         />
+
+                         <x-inputs.text
+                         id="country"
+                         name="country"
+                         placeholder="Spain"
+                         label="Country"
+                         maxlength="100"
+                         value="{{ $client->country }}"
+                         />
+
+                        <x-inputs.text 
+                        id="city"
+                        name="city"
+                        placeholder="Madrid"
+                        label="City"
+                        maxlength="100"
+                        value="{{ $client->city }}"
+                        />
+
+                        <x-inputs.file 
+                        id="logo"
+                        name="logo_path"
+                        label="Company Logo"
+                        />
+
+                    </x-dashboard.forms.form-block>
+
+                    <x-dashboard.forms.form-block label="Primary contact">
+                        <x-inputs.text
+                        id="contact_name"
+                        name="contact_name"
+                        placeholder="Carlos Higuera"
+                        label="Contact name"
+                        required="true"
+                        maxlength="120"
+                        value="{{ $client->contact_name }}"
+                        />
+
+                        <x-inputs.text
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="carlos@mogulzmedia.com"
+                        label="Email address"
+                        required="true"
+                        maxlength="254"
+                        value="{{ $client->email }}"
+                        />
+
+                        <x-inputs.text
+                        id="phone"
+                        name="phone"
+                        type="number"
+                        placeholder="+34 678 948 374"
+                        label="Phone number"
+                        maxlength="25"
+                        value="{{ $client->phone }}"
+                        />
+
+                        <x-inputs.select
+                        id="job_title"
+                        name="job_title"
+                        label="Job title"
+                        default="Select a job title"
+                        :options="[
+                            'ceo' => 'Chief Executive Officer (CEO)',
+                            'founder' => 'Founder',
+                            'co-founder' => 'Co-Founder',
+                            'owner' => 'Owner',
+                            'cmo' => 'Chief Marketing Officer (CMO)',
+                            'marketing_manager' => 'Marketing Manager',
+                            'paid_media_manager' => 'Paid Media Manager',
+                            'social_media_manager' => 'Social Media Manager',
+                            'brand_manager' => 'Brand Manager',
+                            'sales_manager' => 'Sales Manager',
+                            'product_manager' => 'Product Manager (PM)',
+                            'creative_director' => 'Creative Director',
+                            'other' => 'Other'
+                        ]"
+                        />
+                    </x-dashboard.forms.form-block>
+                    
+                    <x-dashboard.forms.form-block
+                    gap="2" label="Client status" type="radio"
+                    radioName="client_status"
+                    >
+                        <div class="flex items-center gap-6">
+                            <x-inputs.radio 
+                            id="onboarding"
+                            name="client_status"
+                            value="onboarding"
+                            title="Onboarding"
+                            icon="fas fa-clipboard"
+                            message="Your client is still in the onboarding phase."
+                            required="true"
+                            />
+
+                            <x-inputs.radio 
+                            id="active"
+                            name="client_status"
+                            value="active"
+                            title="Active"
+                            icon="fas fa-chart-line"
+                            message="You’re already working with the client."
+                            required="true"
+                            />
+                        </div>
+
+                        <div class="flex items-center gap-6">
+                            <x-inputs.radio 
+                            id="paused"
+                            name="client_status"
+                            value="paused"
+                            title="Paused"
+                            icon="fas fa-pause"
+                            message="Client paused for future work."
+                            required="true"
+                            />
+
+                            <x-inputs.radio 
+                            id="draft"
+                            name="client_status"
+                            value="draft"
+                            title="Draft"
+                            icon="fas fa-box"
+                            message="Make a draft to save it for later."
+                            required="true"
+                            />
+                        </div>
+                    </x-dashboard.forms.form-block>
+
+                    <x-dashboard.forms.form-block label="Budget and schedule">
+                        <x-inputs.text
+                        id="start_date"
+                        name="start_date"
+                        type="date"
+                        placeholder="03 / 08 2026"
+                        label="Start Date"
+                        value="{{ $client->start_date }}"
+                        />
+
+                        <x-inputs.select 
+                        id="currency"
+                        name="currency"
+                        label="Currency"
+                        required="true"
+                        default="Select a currency"
+                        :options="[
+                            'USD' => 'US Dollar ($)',
+                            'EUR' => 'Euro (€)',
+                            'GBP' => 'British Pound (£)',
+                            'CHF' => 'Swiss Franc (CHF)',
+                            'IRR' => 'Iranian Rial (﷼)',
+                            'CAD' => 'Canadian Dollar (CA$)',
+                            'AUD' => 'Australian Dollar (A$)',
+                            'NZD' => 'New Zealand Dollar (NZ$)',
+                            'JPY' => 'Japanese Yen (¥)',
+                            'CNY' => 'Chinese Yuan (CN¥)',
+                            'INR' => 'Indian Rupee (₹)',
+                            'SGD' => 'Singapore Dollar (S$)',
+                            'HKD' => 'Hong Kong Dollar (HK$)',
+                            'AED' => 'UAE Dirham (AED)',
+                            'SAR' => 'Saudi Riyal (SAR)',
+                            'SEK' => 'Swedish Krona (SEK)',
+                            'NOK' => 'Norwegian Krone (NOK)',
+                            'DKK' => 'Danish Krone (DKK)',
+                            'MXN' => 'Mexican Peso (MX$)',
+                            'BRL' => 'Brazilian Real (R$)',
+                            'PLN' => 'Polish Złoty (zł)'
+                        ]"
+                        />
+
+                        <x-inputs.text 
+                        id="monthly_budget"
+                        name="monthly_budget"
+                        placeholder="3.840€"
+                        type="number"
+                        label="Monthly budget"
+                        value="{{ $client->monthly_budget }}"
+                        />
+                    </x-dashboard.forms.form-block>
+                    <x-dashboard.forms.form-block 
+                        radioName="advertising_platforms"
+                        gap="2" label="Advertising platforms">
+                        <div class="flex items-center gap-6">
+                            <x-inputs.radio 
+                            id="meta"
+                            name="advertising_platforms"
+                            value="meta"
+                            title="Meta Ads"
+                            icon="fab fa-meta"
+                            />
+
+                            <x-inputs.radio 
+                            id="google"
+                            name="advertising_platforms"
+                            value="google"
+                            title="Google Ads"
+                            icon="fab fa-google"
+                            />
+                        </div>
+                        <div class="flex items-center gap-6">
+                            <x-inputs.radio 
+                            id="tiktok"
+                            name="advertising_platforms"
+                            value="tiktok"
+                            title="TikTok Ads"
+                            icon="fab fa-tiktok"
+                            />
+
+                            <x-inputs.radio 
+                            id="linkedin"
+                            name="advertising_platforms"
+                            value="linkedin"
+                            title="Linkedin Ads"
+                            icon="fab fa-linkedin"
+                            />
+                        </div>
+                    </x-dashboard.forms.form-block>
+                </div>
+                <x-dashboard.forms.summary 
+                summaryTitle="Client Summary"
+                publishTitle="Create Client"
+                />
+            </form>
+        </div>
+    </x-dashboard.main>
+</x-dashboard-layout>
