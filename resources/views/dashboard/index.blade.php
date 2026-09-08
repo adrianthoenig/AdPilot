54df2880c2ec5e9c3f8658abe4a45b65f7731169
Adrian Thoenig <atoficial2016@gmail.com>
2026-09-08 10:49:07 +0200

    add dashboard shell to dashboard index

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,5 +1,7 @@
 <x-dashboard-layout>
     <x-dashboard.main title="Business Overview">
-        
+        <x-dashboard.shell title="Business Overview">
+
+        </x-dashboard.shell>
     </x-dashboard.main>
 </x-dashboard-layout>
